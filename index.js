96ea27fae476ac151ef1f75790cfcd3c6a5b9ab5
Vladyslav <fareomg@gmail.com>
2022-12-21 04:36:34 +0200

    test

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const test = require('./hello.js')
+const { checkOne } = require('./hello.js')
 
 console.log('hello world')
+
+checkOne()
 
 const dom, contactDetails, phonesTemplate, emailsTemplate, dom;
 
 if (contactDetails === NaN) {
   console.log('omg')
+}
+
+function createObjectWrite() {
+  const assignment = `obj[${JSON.stringify(key)}]=42`;
+  return `(function(){${assignment}})` // NOT OK
 }
