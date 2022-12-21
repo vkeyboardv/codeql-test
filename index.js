9c3734b8049b5c30b507681c0ec10e91f2b04c2f
Vladyslav <fareomg@gmail.com>
2022-12-21 04:17:19 +0200

    temp

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const test = require('./hello.js')
 
 console.log('hello world')
+
+const dom, contactDetails, phonesTemplate, emailsTemplate, dom;
+
+if (contactDetails === NaN) {
+  console.log('omg')
+}
